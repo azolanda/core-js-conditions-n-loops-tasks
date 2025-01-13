8d2e449abbfb679f4a749265759413e4316f81da
azolanda <daemonzhilkevich@gmail.com>
2025-01-14 00:45:55 +0300

    feat: add 8 tasks

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -228,8 +228,14 @@ function convertNumberToString(numberStr) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  const strCenter = str.length / 2;
+  for (let i = 0; i < strCenter; i += 1) {
+    if (str[i] !== str[str.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
